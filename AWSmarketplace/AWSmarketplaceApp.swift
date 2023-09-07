247c9c40125e5e84f89853a8df55cf3341e8a1f3
PedroToledo1 <59672144+PedroToledo1@users.noreply.github.com>
2023-09-06 22:48:38 -0300

    Initial Commit

---
+++ AWSmarketplace/AWSmarketplaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AWSmarketplaceApp.swift
+//  AWSmarketplace
+//
+//  Created by Pedro Toledo on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct AWSmarketplaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
